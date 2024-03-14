92c311ab197e7a748f8a85290d09e42f22a538e5
mmohamedkhaled <144614579+mmohamedkhaled@users.noreply.github.com>
2024-03-14 19:58:34 +0200

    modified delay

--- Circuit Classes/sim/sim.cpp
+++ Circuit Classes/sim/sim.cpp
@@ -10,20 +10,20 @@ struct wire
 {
     string name;
     int type;
-    int delay; 
-    int intial;
-    wire(string n, int t, int d) : name(n), type(t), delay(d) {}
+    int delay;
+    int initial;
+    wire(string n, int t, int d) : name(n), type(t), delay(d), initial(0) {}
     void settype(int t)
     {
         type = t;
     }
     void setinitial(int i)
     {
-        intial=i;
+        initial = i;
     }
-    int getintial()
+    int getinitial()
     {
-        return intial;
+        return initial;
     }
 };
 
@@ -46,37 +46,34 @@ bool function(vector<pair<string, vector<wire>>> vec, int duration)
 {
     auto startTime = std::chrono::steady_clock::now();
     bool output = false;
-    
+
     while (true)
     {
         auto currentTime = std::chrono::steady_clock::now();
-        auto elapsedTime = std::chrono::duration_cast<std::chrono::nanoseconds>(currentTime - startTime).count(); // Using nanoseconds for more precise timing
+        auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - startTime).count(); 
 
-// we need to have agreement on the seconds (n/p/m) secounds
         for (auto it = vec.begin(); it != vec.end(); it++)
         {
-            for (int i = 0; i < it->second.size(); i++) 
+            for (int i = 0; i < it->second.size(); i++)
             {
-                wire& currentWire = it->second[i];
-                
+                wire &currentWire = it->second[i];
+
                 if (elapsedTime >= currentWire.delay)
                 {
-                    if(currentWire.getintial()==0)
+                    if (currentWire.getinitial() == 0)
                     {
                         currentWire.setinitial(1);
                     }
                     else
                     {
-                        cout<<currentWire.delay<<" "<<currentWire.name<<" "<<currentWire.type<<endl; 
-                        // outputs only after the first event
-
+                        cout << currentWire.delay << " " << currentWire.name << " " << currentWire.type << endl;
                     }
                     currentWire.settype(1 - currentWire.type);
-                    currentWire.delay += currentWire.delay; // we need to agree on this 
+                    currentWire.delay += currentWire.delay; 
                 }
-                
-                // if(currentWire.type == 1) // might be needed 
-                //     output = true;
+
+                if (currentWire.type == 1) 
+                    output = true;
             }
         }
 
@@ -110,23 +107,23 @@ bool function(vector<pair<string, vector<wire>>> vec, int duration)
             {
                 it->second[0].type = getwire(vec, it->second[1].name) & (getwire(vec, it->second[2].name) & getwire(vec, it->second[3].name));
             }
-            else if(it->first=="OR3")
+            else if (it->first == "OR3")
             {
-                it->second[0].type= getwire(vec, it->second[1].name) | (getwire(vec, it->second[2].name) | getwire(vec, it->second[3].name));
+                it->second[0].type = getwire(vec, it->second[1].name) | (getwire(vec, it->second[2].name) | getwire(vec, it->second[3].name));
             }
-            else if(it->first=="NAND3")
+            else if (it->first == "NAND3")
             {
-                it->second[0].type= !(getwire(vec, it->second[1].name) & (getwire(vec, it->second[2].name) & getwire(vec, it->second[3].name)));
+                it->second[0].type = !(getwire(vec, it->second[1].name) & (getwire(vec, it->second[2].name) & getwire(vec, it->second[3].name)));
             }
-            else if(it->first=="NOR3")
+            else if (it->first == "NOR3")
             {
-                it->second[0].type=!(getwire(vec, it->second[1].name) | (getwire(vec, it->second[2].name) | getwire(vec, it->second[3].name)));
+                it->second[0].type = !(getwire(vec, it->second[1].name) | (getwire(vec, it->second[2].name) | getwire(vec, it->second[3].name)));
             }
             output = it->second[0].type;
         }
         cout << output << endl;
 
-        if (elapsedTime >= duration)
+        if (elapsedTime >= duration * 1000) 
         {
             break;
         }
@@ -135,14 +132,12 @@ bool function(vector<pair<string, vector<wire>>> vec, int duration)
     return output;
 }
 
-
 int main()
 {
     wire A("A", 1, 100); 
-    wire B("B", 1, 400); 
+    wire B("B", 1, 400);
     vector<pair<string, vector<wire>>> vec = {{"NOT", {wire("w1", 0, 0), A}}};
-    // need to insert the delay of the NOT of w1 (calculate it before wala after)
-    function(vec, 10000); 
+    function(vec, 1); 
 
     return 0;
 }
